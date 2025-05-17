d47fc925909b78eb60137eb09bd890bff53110da
CA2AMEL-D0G <207780985+CA2AMEL-D0G@users.noreply.github.com>
2025-05-17 19:02:40 -0300

    adicionei o sistema de trocar telas e a tela principal

--- app/src/main/java/qi/projeto/whatev/clickpark/MainActivity.kt
+++ app/src/main/java/qi/projeto/whatev/clickpark/MainActivity.kt
@@ -9,15 +9,55 @@ import qi.projeto.whatev.clickpark.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    var creditos = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
-
-
-
-
-
         super.onCreate(savedInstanceState)
         this.binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(this.binding.root)
 
+        val homeFragment = HomeFragment()
+        val recargaFragment = RecargaFragment()
+        val adicionarFragment = RecargaFragment()
+        supportFragmentManager.beginTransaction().apply {
+            replace(binding.flfragmentholder.id,homeFragment)
+            commit()
+        }
+
+        binding.btnHome.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(binding.flfragmentholder.id,homeFragment)
+                commit()
+            }
+        }
+        binding.btnRecarga.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(binding.flfragmentholder.id,recargaFragment)
+                commit()
+            }
+        }
+        binding.btnAdicionar.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(binding.flfragmentholder.id,adicionarFragment)
+                commit()
+            }
+        }
+
+
+
+
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
